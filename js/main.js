bcde9d7b077a95a849d72761de952730b8017a8e
Merza-onlile <88036138+Merza-onlile@users.noreply.github.com>
2021-08-20 13:36:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var currentFloor = 2;
+    var floorPath = $('.home-img path');
+    var counterUp = $('.counter-up');
+    var counterDown = ('.counter-down');
+    floorPath.on('mouseover', function () {
+        floorPath.removeClass('current-floor');
+        currentFloor = $(this).attr('data-floor');
+        $('.counter').text(currentFloor);
+    });
+
+
+    counterUp.on('click', function () {
+        if (currentFloor < 16) {
+            currentFloor++;
+            usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false });
+            $('counter').text(usCurrentFloor);
+            floorPath.removeClass('current-floor');
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+        }
+    }); 
+
+    counterDown.on('clicl', function () {
+        if(currentFloor > 2) {
+            currentFloor--;
+            usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false });
+            $('.counter').text(usCurrentFloor);
+            floorPath.removeClass('current-floor');
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+        }
+    });
+});
